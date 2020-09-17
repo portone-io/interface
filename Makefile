1241c210d4167596c0e41f4c7a5c47be55f69bdb
JoowonYun <iam.yunjoowon@gmail.com>
2020-09-17 13:59:57 +0900

    feat: support to dart & cpp

--- Makefile
+++ Makefile
@@ -3,13 +3,15 @@ SRC = src/authenticate/token.proto src/payment/payment.proto
 .PHONY: build
 build:
 	make clean
-	mkdir ./build ./build/csharp ./build/go ./build/java ./build/js ./build/php ./build/python ./build/ts
+	mkdir ./build ./build/csharp ./build/go ./build/java ./build/js ./build/php ./build/python ./build/ts ./build/dart ./build/cpp
 	protoc --proto_path=src --csharp_out=build/csharp $(SRC)
 	protoc --proto_path=src --go_out=build/go --go_opt=paths=source_relative $(SRC)
 	protoc --proto_path=src --java_out=build/java $(SRC)
 	protoc --proto_path=src --js_out=import_style=commonjs,binary:build/js --ts_out=build/ts $(SRC)
 	protoc --proto_path=src --php_out=build/php $(SRC)
 	protoc --proto_path=src --python_out=build/python $(SRC)
+	protoc --proto_path=src --dart_out=build/dart $(SRC)
+	protoc --proto_path=src --cpp_out=build/cpp $(SRC)
 
 .PHONY: clean
 clean:
---
+++ build/dart/authenticate/token.pbjson.dart
@@ -0,0 +1,33 @@
+///
+//  Generated code. Do not modify.
+//  source: authenticate/token.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const Token$json = const {
+  '1': 'Token',
+  '2': const [
+    const {'1': 'access_token', '3': 1, '4': 1, '5': 9, '10': 'accessToken'},
+    const {'1': 'now', '3': 2, '4': 1, '5': 5, '10': 'now'},
+    const {'1': 'expired_at', '3': 3, '4': 1, '5': 5, '10': 'expiredAt'},
+  ],
+};
+
+const TokenRequest$json = const {
+  '1': 'TokenRequest',
+  '2': const [
+    const {'1': 'imp_key', '3': 1, '4': 1, '5': 9, '10': 'impKey'},
+    const {'1': 'imp_secret', '3': 2, '4': 1, '5': 9, '10': 'impSecret'},
+  ],
+};
+
+const TokenResponse$json = const {
+  '1': 'TokenResponse',
+  '2': const [
+    const {'1': 'code', '3': 1, '4': 1, '5': 5, '10': 'code'},
+    const {'1': 'message', '3': 2, '4': 1, '5': 9, '10': 'message'},
+    const {'1': 'response', '3': 3, '4': 1, '5': 11, '6': '.authenticate.Token', '10': 'response'},
+  ],
+};
+
